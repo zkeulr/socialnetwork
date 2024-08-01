3bf33dc451718e857c8aa8ef752092a07c896f35
Ezekiel Ulrich <ezekielulrich@gmail.com>
2024-08-01 13:14:35 -0400

    Formatting

--- src/visualize.py
+++ src/visualize.py
@@ -22,10 +22,12 @@ def visualize():
         edge_y.extend([y0, y1, None])
 
     edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines')
+        x=edge_x,
+        y=edge_y,
+        line=dict(width=0.5, color="#888"),
+        hoverinfo="none",
+        mode="lines",
+    )
 
     node_x = []
     node_y = []
@@ -37,44 +39,48 @@ def visualize():
         node_customdata.append(f"https://www.instagram.com/{node}")
 
     node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers+text',
+        x=node_x,
+        y=node_y,
+        mode="markers+text",
         text=[node for node in G.nodes()],
         customdata=node_customdata,
-        hoverinfo='text',
+        hoverinfo="text",
         marker=dict(
             showscale=True,
-            colorscale='YlGnBu',
+            colorscale="YlGnBu",
             size=10,
             colorbar=dict(
                 thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
+                title="Node Connections",
+                xanchor="left",
+                titleside="right",
             ),
-        )
+        ),
     )
 
-    fig = go.Figure(data=[edge_trace, node_trace],
-                    layout=go.Layout(
-                        title='Social Network',
-                        titlefont_size=16,
-                        showlegend=False,
-                        hovermode='closest',
-                        margin=dict(b=40, l=40, r=40, t=40),
-                        annotations=[dict(
-                            text="Click on a node to visit profile",
-                            showarrow=False,
-                            xref="paper",
-                            yref="paper",
-                            x=0.005,
-                            y=0.002,
-                        )],
-                        xaxis=dict(showgrid=False, zeroline=False),
-                        yaxis=dict(showgrid=False, zeroline=False),
-                        plot_bgcolor='rgba(0,0,0,0)',
-                        paper_bgcolor='rgba(0,0,0,0)',
-                    )
+    fig = go.Figure(
+        data=[edge_trace, node_trace],
+        layout=go.Layout(
+            title="Social Network",
+            titlefont_size=16,
+            showlegend=False,
+            hovermode="closest",
+            margin=dict(b=40, l=40, r=40, t=40),
+            annotations=[
+                dict(
+                    text="Click on a node to visit profile",
+                    showarrow=False,
+                    xref="paper",
+                    yref="paper",
+                    x=0.005,
+                    y=0.002,
+                )
+            ],
+            xaxis=dict(showgrid=False, zeroline=False),
+            yaxis=dict(showgrid=False, zeroline=False),
+            plot_bgcolor="rgba(0,0,0,0)",
+            paper_bgcolor="rgba(0,0,0,0)",
+        ),
     )
 
     fig.write_html("vars/visualization.html")
